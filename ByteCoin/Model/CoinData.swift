e21946e560e7217c7f53be188b9baff69b29950d
Sumit Mandal <sumitmandal749@gmail.com>
2021-04-30 19:15:44 +0530

    WIP: App completed with an error

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Sumit Mandal on 30/04/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
